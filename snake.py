014f9f5e2fa08954f0392915bb8623909ba04524
ErinManson <0404em@gmail.com>
2024-06-11 10:39:57 -0400

    Developing snake game

--- snake.py
+++ snake.py
@@ -1,1 +1,19 @@
-print("hello")
+#imports 
+import pygame
+import time
+import random
+
+speed = 15
+
+#colours and sizes
+horizontal =720
+vertical = 480
+black = pygame.Color(0,0,0)
+white = pygame.Color(255,255,255)
+red = pygame.Color(255,0,0)
+green = pygame.Color(0,255,0)
+blue = pygame.Color(0,0,255)
+
+pygame.init()
+pygame.display.set
+screen = pygame.display.set_mode((horizontal,vertical))
